a156b3f959102a754d2860c91ad79a7e21ff3c73
Jon Valdés <juanval@gmail.com>
2014-08-08 12:15:00 +0200

    Now full logcat is served via http port 10001 so big logs aren't lost due to terminal limitations and so you can search

--- main.go
+++ main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"os"
+	"sync"
+	"time"
 )
 
 func crashIf(err error) {
@@ -17,6 +21,7 @@ func crashIf(err error) {
 // We store the adb output. In the future we'd like
 // to be able to save/process the whole adb history
 var lines []string
+var linesMutex sync.Mutex
 
 func main() {
 	readFilters("filters.yaml")
@@ -25,11 +30,50 @@ func main() {
 	adbChan := make(chan string, 10000)
 	crashIf(readAdb(adbChan))
 
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		lineNum := 0
+		var l string
+
+		for {
+			linesMutex.Lock()
+			mustSend := lineNum < len(lines)
+			if mustSend {
+				l = lines[lineNum]
+				lineNum++
+			}
+			linesMutex.Unlock()
+
+			if !mustSend {
+				time.Sleep(50 * time.Millisecond)
+				continue
+			}
+
+			fmt.Fprintln(w, l)
+			if flusher, ok := w.(http.Flusher); ok {
+				flusher.Flush()
+			}
+		}
+	}
+
+	server := http.Server{
+		Addr:         ":10001",
+		WriteTimeout: 4 * time.Hour,
+		Handler:      http.HandlerFunc(handler),
+	}
+
+	go func() {
+		log.Fatal(server.ListenAndServe())
+	}()
+
 	for {
 		line := <-adbChan
+		linesMutex.Lock()
 		lines = append(lines, line)
+		linesMutex.Unlock()
+
 		if mustPrint(line) {
 			fmt.Println(highlightString(line))
 		}
 	}
+
 }
